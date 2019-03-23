95309353381548f575addc8f8bcfa8732efba3cb
Lamierus <lamierus@gmail.com>
2019-03-23 16:27:13 -0400

    refactoring to use the Console Game Engine properly Found out i wasn't using it correctly, as i finally looked at the samples.  HURR DURR. Hopefully i'll have an easier time getting this running and working out nicely. :)

---
+++ RogueLike/RogueLike.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ConsoleGameEngine;
+
+namespace RogueLike {
+    class RogueLike : ConsoleGame {
+
+        /// <summary>
+        /// ConsoleCharacter
+        ///     .Null - ?
+        ///     .Light - 1/3 pixels
+        ///     .Medium - 1/2 pixels
+        ///     .Dark - 3/4 pixels
+        ///     .BoxDrawingL_XX
+        ///         drawing boxes with pixels, each of the last 2 characters explain the positioning of the lines in the pixel
+        /// </summary>
+        //private static ConsoleEngine Engine;
+
+        const int c_SideBar = 40;
+        const int c_WinWidth = 170;
+        const int c_WinHeight = 50;
+        const int c_PixelWidth = 4;
+        const int c_PixelHeight = 8;
+        const int BlankSpotColor = 0;
+        const ConsoleKey keyClose = ConsoleKey.Escape;
+
+        public Player You = new Player();
+        public Position PlayerLastPosition;
+        public Map CurrentLevel = new Map();
+        public Room CurrentRoom;
+        
+        private bool GameState = true;
+        private Random RandomNum = new Random();
+
+        static void Main(string[] args) {
+            new RogueLike().Construct(c_WinWidth, c_WinHeight, c_PixelWidth, c_PixelHeight, FramerateMode.MaxFps);
+        }
+
+        public override void Create() {
+            Engine.SetPalette(Palettes.Default);
+            Engine.SetBackground(0);
+            Console.Title = "Ummm... Isn't this Snake?  SNAKE!...  SNNNAAAAAAKKKKEEEE!!!";
+            TargetFramerate = 60;
+
+            CreateWalls();
+            AddItems();
+            DrawSideBar();
+            Restart();
+        }
+
+        /// <summary>
+        ///     currently only draws the rectangle around the edge, but can be modified to add "doorways" by blanking out sections,
+        ///     depending on coordinates from a "map"
+        /// </summary>
+        void CreateWalls() {
+            Engine.Rectangle(new Point(0, 0), new Point(c_WinWidth - c_SideBar - 1, c_WinHeight - 1), 2, ConsoleCharacter.Light);
+        }
+
+        void AddItems() {
+            var pntItem = new Position(0,0);
+            var itemAmount = RandomNum.Next(1, 99);
+            var newRoom = new Room();
+            RandomizePixelPoint(ref pntItem);
+            Gold item = new Gold(itemAmount, pntItem);
+            newRoom.ItemsInRoom.Add(item);
+            Engine.WriteText(item.XY.ToPoint(), item.Character.ToString(), item.Color);
+            CurrentLevel.Rooms.Add(newRoom);
+            CurrentRoom = newRoom;
+        }
+
+        void DrawSideBar() {
+            Position GoldCounter = new Position(c_WinWidth - c_SideBar + 2, 0);
+            Engine.WriteText(GoldCounter.ToPoint(), "Gold: " + You.GoldAmt, 3);
+        }
+
+
+        /// <summary>
+        ///     
+        /// </summary>
+        void UpdateSideBar() {
+            DrawSideBar();
+        }
+
+        public override void Update() {
+            if (GameState) {
+                if (!(Engine.GetKey(keyClose))) {
+
+                    ConsoleKeyInfo keyPress;
+                    int moveX = 0, moveY = 0;
+                    if (Console.KeyAvailable) {
+                        keyPress = Console.ReadKey();
+                        moveX = 0;
+                        moveY = 0;
+                        switch (char.ToUpper(keyPress.KeyChar)) {
+                            case 'W':
+                                moveY = -1;
+                                break;
+                            case 'A':
+                                moveX = -1;
+                                break;
+                            case 'S':
+                                moveY = 1;
+                                break;
+                            case 'D':
+                                moveX = 1;
+                                break;
+                        }
+                    }
+                    if ((You.XY.X + moveX < c_WinWidth - c_SideBar - 1) && (You.XY.X + moveX > 0))
+                        You.XY.X += moveX;
+                    if ((You.XY.Y + moveY < c_WinHeight - 1) && (You.XY.Y + moveY > 0))
+                        You.XY.Y += moveY;
+                    foreach (Item i in CurrentRoom.ItemsInRoom) {
+                        if (You.XY == i.XY) {
+                            if (You.PickUpItem(i))
+                                //CurrentRoom.ItemsInRoom.RemoveAt(CurrentRoom.ItemsInRoom.IndexOf(i));
+                            UpdateSideBar();
+                        }
+                    }
+                } else {
+                    GameState = false;
+                }
+            } else {
+                Restart();
+            }
+        }
+
+        public override void Render() {
+            Engine.WriteText(You.XY.ToPoint(), You.Character.ToString(), You.RGBColor.GetHashCode());
+            //Engine.SetPixel(You.XY.ToPoint(), You.RGBColor.GetHashCode(), ConsoleCharacter.Full);
+            Engine.DisplayBuffer();
+            Engine.SetPixel(PlayerLastPosition.ToPoint(), BlankSpotColor, ConsoleCharacter.Full);
+            PlayerLastPosition = You.XY;
+        }
+
+        void Restart() {
+            GameState = true;
+            You.XY = new Position(10, 10);
+            PlayerLastPosition = You.XY;
+        }
+        
+        /// <summary>
+        ///     Used this for testing, but may find a use for it in the future
+        /// </summary>
+        /// <param name="position">starting point, to be changed to another random point</param>
+        void RandomizePixelPoint(ref Position position) {
+            var newPosition = new Position();
+            do {
+                newPosition.X = RandomNum.Next(1, c_WinWidth - c_SideBar - 1);
+                newPosition.Y = RandomNum.Next(1, c_WinHeight - c_SideBar - 1);
+            } while (newPosition == position);
+            position = newPosition;
+        }
+
+        /// <summary>
+        ///     Used for testing, but may find a use for it in the future
+        /// </summary>
+        /// <param name="color">color of the point, which will be changed</param>
+        void RandomizePixelColor(ref int color) {
+            var newColor = 0;
+            do {
+                newColor = RandomNum.Next(0, 256);
+            } while (newColor == color);
+            color = newColor;
+        }
+    }
+}
