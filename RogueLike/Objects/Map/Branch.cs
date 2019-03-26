b3f3f71d9dca522c38746969fc3e7461eceefb9f
Lamierus <lamierus@gmail.com>
2019-03-26 11:45:07 -0400

    Starting procedural dungeon creation I've found a couple of algorithms online, and I'm going to try to build on myself from my understanding of the BPS branching system.  I want this project to be as much of my own code as possible.

---
+++ RogueLike/Objects/Map/Branch.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueLike {
+    public class Branch : Tree {
+        private int MaxRoomLength;
+        private int MaxRoomWidth;
+    }
+}
---
+++ RogueLike/Objects/Map/Tree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RogueLike {
+    public class Tree {
+        private Branch LeftBranch;
+        private Branch RightBranch;
+
+        private int Length;
+        private int Width;
+        public int MaxBranches { get; set; }
+    }
+}
