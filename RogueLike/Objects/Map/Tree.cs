e37b91355eaf7c3fd95029753e53bf62596f2f00
Lamierus <lamierus@gmail.com>
2019-03-27 16:08:37 -0400

    Procedural stuff I'm getting it figured out... slowly... ugh.

--- RogueLike/Objects/Map/Tree.cs
+++ RogueLike/Objects/Map/Tree.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace RogueLike {
-    public class Tree {
-        private Branch LeftBranch;
-        private Branch RightBranch;
+    public class Dungeon {
+        private const int c_MinSize = 10;
         private Random Rand = new Random();
-        private int Width;
-        private int Height;
+        private Dungeon LeftBranch;
+        private Dungeon RightBranch;
+        private int Width, Height;
+        private Position TopLeft;
+        public bool Split;
         public int MaxBranches;
 
-        public Tree(int width = 5, int height = 5, int maxBranches = 1) {
-            MaxBranches = maxBranches;
+        public Dungeon(int width, int height, int top, int left) {
             Width = width;
             Height = height;
-            GenerateBranches();
+            TopLeft = new Position(top, left);
         }
 
         private void GenerateBranches() {
@@ -24,13 +25,9 @@ namespace RogueLike {
             for (int i = 0; i < MaxBranches; i++) {
                 VorH = Rand.Next(0, 1);
                 if (VorH > 0) {
-                    max = Rand.Next(1, (Width - (Width % MaxBranches)));
-                    LeftBranch = new Branch(Width, max);
-                    RightBranch = new Branch(Width, Height - max);
+                    
                 } else {
-                    max = Rand.Next(1, (Height - (Height % MaxBranches)));
-                    LeftBranch = new Branch(max, Height);
-                    RightBranch = new Branch(Width - max, Height);
+                    
                 }
             }
         }
