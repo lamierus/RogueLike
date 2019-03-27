bd6c21a8a95335de5f4d28c8b0a525da142309f5
Lamierus <lamierus@gmail.com>
2019-03-26 21:42:52 -0400

    I'm finding my way... BUT I HAVE NO IDEA WHAT I'M DOING. I'll figure it out somehow.  I'll have to refer back to the BSP algorithms, though... i'm sure i'm way off.

--- RogueLike/Objects/Map/Branch.cs
+++ RogueLike/Objects/Map/Branch.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace RogueLike {
-    public class Branch : Tree {
-        private int MaxRoomLength;
+    public class Branch{
+        private Branch LeftBranch;
+        private Branch RightBranch;
+        private Random Rand;
+        private int Width;
+        private int Height;
         private int MaxRoomWidth;
+        private int MaxRoomHeight;
+
+        public Branch(int width, int height) {
+            Width = width;
+            MaxRoomWidth = width - 1;
+            Height = height;
+            MaxRoomHeight = Height - 1;
+        }
+        
     }
 }
--- RogueLike/Objects/Map/Tree.cs
+++ RogueLike/Objects/Map/Tree.cs
@@ -6,9 +6,33 @@ namespace RogueLike {
     public class Tree {
         private Branch LeftBranch;
         private Branch RightBranch;
-
-        private int Length;
+        private Random Rand = new Random();
         private int Width;
-        public int MaxBranches { get; set; }
+        private int Height;
+        public int MaxBranches;
+
+        public Tree(int width = 5, int height = 5, int maxBranches = 1) {
+            MaxBranches = maxBranches;
+            Width = width;
+            Height = height;
+            GenerateBranches();
+        }
+
+        private void GenerateBranches() {
+            int VorH;
+            int max;
+            for (int i = 0; i < MaxBranches; i++) {
+                VorH = Rand.Next(0, 1);
+                if (VorH > 0) {
+                    max = Rand.Next(1, (Width - (Width % MaxBranches)));
+                    LeftBranch = new Branch(Width, max);
+                    RightBranch = new Branch(Width, Height - max);
+                } else {
+                    max = Rand.Next(1, (Height - (Height % MaxBranches)));
+                    LeftBranch = new Branch(max, Height);
+                    RightBranch = new Branch(Width - max, Height);
+                }
+            }
+        }
     }
 }
